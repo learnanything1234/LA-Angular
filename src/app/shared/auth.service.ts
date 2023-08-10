e71d75581389b7dea56e1b20149b3e3e6c68d6de
Rob Percival <rob@blutick.com>
2023-08-10 07:26:55 +0100

    Revert "swapped bootstrap for tailwind"
    This reverts commit 5428091d4a0daea82adb0dc1e84de7e4ab4f9231.

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,56 +1,72 @@
+import { environment } from './../../environments/environment';
+ 
+ 
+ 
 import { Injectable } from '@angular/core';
-
-import { GoogleAuthProvider, OAuthProvider, browserSessionPersistence } from 'firebase/auth';
-
+ 
+import { GoogleAuthProvider, OAuthProvider, getAuth} from 'firebase/auth';
+ 
 import { Router } from '@angular/router';
-import {
-  Auth, createUserWithEmailAndPassword,
+import { Observable, map, switchMap } from 'rxjs';
+import {Auth, createUserWithEmailAndPassword,
   signInWithPopup,
   sendPasswordResetEmail,
   signOut,
   sendEmailVerification,
-  signInWithEmailAndPassword
-} from '@angular/fire/auth';
-
-
+  signInWithEmailAndPassword}  from '@angular/fire/auth';
+import {
+  collection,
+  doc,
+  getFirestore,
+  getDoc,
+  Firestore,
+ 
+  collectionData,
+ 
+  
+} from '@angular/fire/firestore';
+import { addDoc } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+ 
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  userid: any;
+  userid:any ;
 
-  constructor(private auth: Auth, private router: Router) { auth.setPersistence(browserSessionPersistence) }
+  constructor( private fs:Firestore,private auth:Auth,   private router : Router) { }
 
   // login method
-  login(email: string, password: string) {
-    signInWithEmailAndPassword(this.auth, email, password).then((res: any) => {
-      localStorage.setItem('token', 'true');
+  login(email : string, password : string) {
+    signInWithEmailAndPassword(this.auth,email,password).then( (res:any) => {
+        localStorage.setItem('token','true');
 
-      if (res.user?.emailVerified == true) {
-        this.router.navigate(['dashboard']);
-      } else {
-        this.router.navigate(['/varify-email']);
-      }
+        if(res.user?.emailVerified == true) {
+          this.router.navigate(['dashboard']);
+        } else {
+          this.router.navigate(['/varify-email']);
+        }
 
     }, (err: { message: any; }) => {
-      alert(err.message);
-      this.router.navigate(['/login']);
+        alert(err.message);
+        this.router.navigate(['/login']);
     })
   }
 
+  
+  
 
-
-
-
+ 
 
   // async  getUserById() {
   //   const app = initializeApp(environment.firebaseConfig);
   //   const db = getFirestore(app);
   //   const auth = getAuth(app);
-
+  
   //   try {
   //     const user = auth.currentUser;
-
+  
   //     if (user) {
   //       const documentRef = doc(db, '/users', user.uid);
   //       const docSnapshot = await getDoc(documentRef);
@@ -69,13 +85,13 @@ export class AuthService {
   //   }
   // }
   // register method
-  register(email: string, password: string) {
+  register(email : string, password : string) {
 
-    createUserWithEmailAndPassword(this.auth, email, password).then((res: any) => {
+    createUserWithEmailAndPassword(this.auth,email, password).then( (res:any) => {
       alert('Registration Successful');
       this.sendEmailForVarification(res.user);
-
-      //  this.addUserInfoToCollection(res.user.uid, email);
+      
+        //  this.addUserInfoToCollection(res.user.uid, email);
       this.router.navigate(['/login']);
     }, (err: { message: any; }) => {
       alert(err.message);
@@ -83,9 +99,23 @@ export class AuthService {
     })
   }
 
+
+
+    // // Function to add user information to Firestore collection
+    //   addUserInfoToCollection(uid: string, email: string) {
+
+    //     const userInfo = {
+    //     uid: uid,
+    //     email: email,
+    //   };
+
+    //      return addDoc(collection(this.fs,'users'),userInfo);
+       
+    // }
+
   // sign out
   logout() {
-    signOut(this.auth).then(() => {
+     signOut(this.auth).then( () => {
       localStorage.removeItem('token');
       this.router.navigate(['/login']);
     }, (err: { message: any; }) => {
@@ -94,44 +124,42 @@ export class AuthService {
   }
 
   // forgot password
-  forgotPassword(email: string) {
-    sendPasswordResetEmail(this.auth, email).then(() => {
-      alert('If you have an account with us, your password reset email has been sent. Please check your inbox.');
-      this.router.navigate(['/login']);
-    }, (err: any) => {
-      alert('If you have an account with us, your password reset email has been sent. Please check your inbox.');
-      this.router.navigate(['/login']);
-    })
+  forgotPassword(email : string) {
+      sendPasswordResetEmail(this.auth,email).then(() => {
+        this.router.navigate(['/varify-email']);
+      }, (err: any) => {
+        alert('Something went wrong');
+      })
   }
 
   // email varification
-  sendEmailForVarification(user: any) {
+  sendEmailForVarification(user : any) {
     console.log(user);
-    sendEmailVerification(user).then((res: any) => {
+     sendEmailVerification(user).then((res : any) => {
       this.router.navigate(['/varify-email']);
-    }, (err: any) => {
+    }, (err : any) => {
       alert('Something went wrong. Not able to send mail to your email.')
     })
   }
 
   //sign in with google
   googleSignIn() {
-    return signInWithPopup(this.auth, new GoogleAuthProvider).then((res: any) => {
+    return  signInWithPopup(this.auth,  new GoogleAuthProvider).then((res:any) => {
 
       this.router.navigate(['/dashboard']);
-      localStorage.setItem('token', JSON.stringify(res.user?.uid));
+      localStorage.setItem('token',JSON.stringify(res.user?.uid));
       // this.addUserInfoToCollection(res.user?.uid, res.user?.email);
 
     }, (err: { message: any; }) => {
       alert(err.message);
     })
   }
-
+  
   //login with microsoft 
   signInWithMicrosoft() {
     console.log("signing in with microsoft...")
-    const microsoftProvider = new OAuthProvider('microsoft.com');
-    signInWithPopup(this.auth, microsoftProvider).then((result) => {
+    const microsoftProvider = new  OAuthProvider('microsoft.com');
+      signInWithPopup(this.auth,microsoftProvider).then((result) => {
       // Handle successful login
       console.log(result);
       this.router.navigate(['/dashboard']);
